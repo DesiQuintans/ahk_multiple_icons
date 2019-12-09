0ac9de29364e1755976b7bf080c28eb3b1a1c991
Desi Quintans <science@desiquintans.com>
2019-12-09 19:55:35 +1100

    Comment each icon for easy editing

--- compile.bat
+++ compile.bat
@@ -1,31 +1,35 @@
-"C:\Dropbox\Autohotkey\Ahk2Exe_L\Ahk2Exe.exe" /in "icon_test.ahk" /icon "icons\ahk_default.ico"
-
 @REM   The TIMEOUTs make time for the executable to be compiled and edited at each step.
 @REM   Without them, some icons might not be overwritten properly.
+
+"C:\Dropbox\Autohotkey\Ahk2Exe_L\Ahk2Exe.exe" /in "icon_test.ahk" /icon "icons\ahk_default.ico"
 @TIMEOUT 5
 
 copy icon_test.exe icon_test_edited.exe
-
 @TIMEOUT 1
 
+@REM Main program icon.
 "C:\Dropbox\Apps\ResHacker\ResourceHacker.exe" -open icon_test_edited.exe -save icon_test_edited.exe -action addoverwrite -resource "icons\up.ico" -mask ICONGROUP,159,
-
 @TIMEOUT 1
 
+
+@REM Script icon.
 "C:\Dropbox\Apps\ResHacker\ResourceHacker.exe" -open icon_test_edited.exe -save icon_test_edited.exe -action addoverwrite -resource "icons\up_right.ico" -mask ICONGROUP,160,
-
 @TIMEOUT 1
 
+
+@REM 'Suspend' icon.
 "C:\Dropbox\Apps\ResHacker\ResourceHacker.exe" -open icon_test_edited.exe -save icon_test_edited.exe -action addoverwrite -resource "icons\right.ico" -mask ICONGROUP,206,
-
 @TIMEOUT 1
 
+
+@REM 'Pause' icon.
 "C:\Dropbox\Apps\ResHacker\ResourceHacker.exe" -open icon_test_edited.exe -save icon_test_edited.exe -action addoverwrite -resource "icons\down_right.ico" -mask ICONGROUP,207,
-
 @TIMEOUT 1
 
+
+@REM 'Suspend' + 'Pause' icon.
 "C:\Dropbox\Apps\ResHacker\ResourceHacker.exe" -open icon_test_edited.exe -save icon_test_edited.exe -action addoverwrite -resource "icons\down.ico" -mask ICONGROUP,208,
-
 @TIMEOUT 1
+
 
 @PAUSE
